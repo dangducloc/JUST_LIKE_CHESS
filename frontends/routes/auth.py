e8de4a9e5844395e30a31533da8fe1351953ef5e
dangducloc <dangloc2110@gmail.com>
2025-11-30 23:57:14 +0700

    [ADD] add UI to fe

--- frontends/routes/auth.py
+++ frontends/routes/auth.py
@@ -1,19 +1,19 @@
 # frontends/routes/auth.py
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template
 
 auth_frontend_bp = Blueprint('auth_frontend', __name__,static_folder='../../static', template_folder='../../templates')
 
 # =========== LOGIN PAGE ============
-@auth_frontend_bp.route('/login', methods=['GET'])
+@auth_frontend_bp.route('/login', methods=['GET'], strict_slashes=False)
 def login_page():
-    return auth_frontend_bp.send_static_file('login.html')
+    return render_template('auth/login.html')
 
 # =========== REGISTER PAGE ============
-@auth_frontend_bp.route('/register', methods=['GET'])
+@auth_frontend_bp.route('/register', methods=['GET'], strict_slashes=False)
 def register_page():
-    return auth_frontend_bp.send_static_file('register.html')
+    return render_template('auth/register.html')
 
 # =========== CONFIRM PAGE ============
-@auth_frontend_bp.route('/confirm/', methods=['GET'])
+@auth_frontend_bp.route('/confirm', methods=['GET'], strict_slashes=False)
 def confirm_page():
-    return auth_frontend_bp.send_static_file('confirm.html')
+    return render_template('auth/confirm.html')
